f0fe1df548affc8c6cb49bf6ce51c08b1acac4da
Ryan Weston <ryanlweston@gmail.com>
2022-08-07 17:39:05 +0100

    feat: update tokens & theme

--- src/theme/defaultTheme.ts
+++ src/theme/defaultTheme.ts
@@ -1,26 +1,4 @@
 export const defaultTheme = {
-  menu: {
-    wrapper: 'bg-interfacePage border-b border-interfaceContrast',
-    screenWrapper: 'max-w-7xl mx-auto px-2 sm:px-6 lg:px-8',
-    screenContainer: 'relative flex items-center justify-between h-16 md:h-24',
-    screenMenuWrapper: 'hidden md:block absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0',
-    screenMenuContainer: 'flex space-x-4',
-    mobileWrapper: 'md:hidden',
-    mobileContainer: 'px-2 pt-2 pb-3 space-y-2 flex-col flex',
-    menuButton: {
-      container: 'absolute inset-y-0 left-0 flex items-center md:hidden',
-      button: 'inline-flex items-center justify-center p-2 rounded-md text-textOne hover:text-textInverse hover:bg-interactivePrimary focus:outline-none focus:ring-2 focus:ring-inset focus:ring-focusOne',
-      icon: 'block h-6 w-6',
-      closeIcon: 'text-textOne',
-    },
-    logo: {
-      wrapper: 'flex-1 flex items-center justify-center md:items-stretch md:justify-start',
-      container: 'flex-shrink-0 flex items-center',
-      text: 'block w-auto font-bold',
-      image: 'hidden md:block h-10 w-auto',
-      mobileImage: 'block md:hidden h-6 w-auto',
-    },
-  },
   button: {
     small: 'px-3 py-2 rounded-md text-sm font-medium inline-flex items-center',
     medium: 'px-4 py-2 rounded-lg inline-flex items-center',
@@ -47,18 +25,6 @@ export const defaultTheme = {
   link: {
     base: 'text-interactiveLink hover:underline',
   },
-  dropdown: {
-    wrapper: 'relative inline-block text-left',
-    transition: {
-      enterActiveClass: 'transition ease-out duration-100',
-      enterFromClass: 'transform opacity-0 scale-95',
-      enterToClass: 'transform opacity-100 scale-100',
-      leaveActiveClass: 'transition ease-in duration-75',
-      leaveFromClass: 'transform opacity-100 scale-100',
-      leaveToClass: 'transform opacity-0 scale-95',
-    },
-    menu: 'origin-top-right absolute right-0 border border-interfaceContrast mt-2 w-56 rounded-md shadow-lg bg-interfaceOne ring-1 ring-focusOne ring-opacity-5 focus:outline-none py-2',
-  },
   footer: {
     wrapper: 'w-full border-t border-interfaceContrast',
     absolute: 'absolute bottom-0 left-0',
@@ -84,10 +50,6 @@ export const defaultTheme = {
       secondary: 'text-iconTwo',
       contrast: 'text-iconThree',
     },
-  },
-  dropdownItem: {
-    base: 'block px-4 py-2 text-sm flex flex-row items-center text-textOne hover:bg-interfaceContrast',
-    disabled: 'opacity-50 cursor-not-allowed pointer-events-none',
   },
   headline: {
     base: 'text-textOne',
--- tokens.js
+++ tokens.js
@@ -1,44 +1,40 @@
 /**
  * Do not edit directly
- * Generated on Sat, 30 Jul 2022 13:33:56 GMT
+ * Generated on Sun, 07 Aug 2022 16:33:42 GMT
  */
 
 module.exports = {
-  textOne: '#ffffff', // Brand text colour. Should contrast both the layout background & the brand background.
+  textOne: '#ffffff', // Default text colour
   brandPrimary: '#ffffff', // Primary brand colour
-  interfaceOne: '#1d1f23', // Layout background colour. Should slightly contrast against brand background. Used for containers.
-  actionError: '#ee4343', // Action error colour. Used to connote error states and negative actions.
-  actionSuccess: '#49b737', // Action success colour. Used to connote a successful or positive action.
-  textTwo: '#70747d', // Brand text contrast colour. Should contrast against the primary colour.
-  interfacePage: '#000000', // No description
-  interfaceTwo: '#2e3137', // No description
-  interfaceContrast: '#4a4d54', // No description
-  interactiveVariant: '#b1b1b1', // Action link colour. Used for external hyperlinks.
-  focusOne: '#0a5fff', // No description
-  actionWarning: '#dd8a3d', // No description
-  textContrast: '#000000', // No description
-  hoverInterface: '#1e1e1e', // No description
-  focusTwo: '#ffffff', // No description
-  activeInterface: '#9a9a9a', // No description
-  disabledBackground: '#1b1b1b', // No description
-  disabledContent: '#494949', // No description
-  hoverPrimary: '#ffffff', // No description
-  hoverPrimarytext: '#ffffff', // No description
-  hoverSecondary: '#ffffff', // No description
-  hoverVariant: '#169d00', // No description
-  hoverVarianttext: '#169d00', // No description
-  activePrimary: '#ffffff', // No description
-  activeSecondary: '#169d00', // No description
-  textPlaceholder: '#535353', // No description
-  interactivePrimary: '#ffffff', // No description
-  interactiveSecondary: '#ffffff', // No description
-  inputOne: '#272727', // No description
-  inputTwo: '#cf2121', // No description
-  inputThree: '#169d00', // No description
-  iconOne: '#ffffff', // No description
-  iconTwo: '#b1b1b1', // No description
-  iconThree: '#169d00', // No description
-  textInverse: '#000000', // No description
-  interactiveLink: '#0068c8', // No description
-  disabledIcon: '#333333', // No description
+  interfaceOne: '#1e2023', // Default page contrast
+  actionError: '#ee4343', // Error element colour
+  actionSuccess: '#49b737', // Success element colour
+  textTwo: '#70747d', // Secondary text colour
+  interfacePage: '#000000', // Default page background
+  interfaceTwo: '#282b2f', // interfaceOne contrast
+  interfaceContrast: '#363a41', // Tertiary background
+  interactiveVariant: '#b1b1b1', // Variant interactive elements
+  focusOne: '#0a5fff', // Focus for border and underline
+  actionWarning: '#dd8a3d', // Warning element colour
+  hoverInterface: '#1e1e1e', // Hover on interface elements
+  focusTwo: '#ffffff', // Focus for high contrast elements
+  activeInterface: '#9a9a9a', // Active interface elements
+  disabledBackground: '#1b1b1b', // Disabled element background
+  disabledContent: '#494949', // Disabled element content
+  hoverPrimary: '#ffffff', // Hover on interactivePrimary
+  hoverSecondary: '#ffffff', // Hover on interactiveSecondary
+  hoverVariant: '#ffffff', // Hover on interactiveVariant
+  activePrimary: '#ffffff', // Active interactivePrimary
+  activeSecondary: '#ffffff', // Active interactiveSecondary
+  textPlaceholder: '#535353', // Placeholder text colour
+  interactivePrimary: '#ffffff', // Primary interactive elements
+  interactiveSecondary: '#ffffff', // Secondary interactive elements
+  inputOne: '#272727', // Default input - contrast interfacePage
+  inputTwo: '#393939', // Contrast interfaceOne & Two
+  iconOne: '#ffffff', // Primary icons
+  iconTwo: '#353535', // Secondary icons
+  iconThree: '#000000', // Icons on interactive colours
+  textInverse: '#000000', // Contrast to interactivePrimary
+  interactiveLink: '#0068c8', // Interactive link colour
+  disabledIcon: '#333333', // Disabled element icon
 }
